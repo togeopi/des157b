64a29231eff0fe1b8073368f9683a91e4b789aff
togeopi <ckgutierrez@ucdavis.edu>
2024-04-04 09:44:19 -0700

    initial push

---
+++ script.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet">
+
+	<link href="styles.css" rel="stylesheet">
+    <title>des157b homepage</title>
+</head>
+
+<body></body>
